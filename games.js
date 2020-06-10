6d42e7212d7f21d87a563e41f433c484a5b046d3
6rak0 <grako@6rakbook.local>
2020-06-10 17:04:06 -0500

    commit inicial

---
+++ games.js
@@ -0,0 +1,20 @@
+const { getGamesAmerica, getPrices } = require('nintendo-switch-eshop');
+
+async function getDiscounted() {
+  const allGames = await getGamesAmerica();
+  const prices = allGames.map(async game => {
+    await getPrices('MX', game.nsuid);
+  });
+
+  console.log(prices);
+  // const query = await getPrices('MX', queryGames[0].nsuid);
+  // console.log(query.prices[0]);
+  // console.log(queryGames[0]);
+  // queryGames.map(async game => {
+  //   let data = await getPrices('MX', game.nsuid);
+  //   data = data.prices[0];
+  //   console.log(data);
+  // });
+}
+
+getDiscounted();
---
+++ index.js
@@ -0,0 +1,15 @@
+const { Telegraf } = require('telegraf');
+const { getGamesAmerica } = require('nintendo-switch-eshop');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start(ctx => ctx.reply('Welcome!'));
+bot.hears('pepito', ctx => ctx.reply('pepitos crew'));
+bot.command('hipster', Telegraf.reply('λ'));
+bot.on('message', ctx => ctx.reply(ctx.message.text));
+bot.hears('nintendo', async ctx => {
+  const allGames = await getGamesAmerica();
+  const query = allGames.filter(game => game.title.includes('Mario Kart'));
+  ctx.reply(query[0].title);
+});
+
+bot.launch();
